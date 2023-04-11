9726f95e6c4c8a7f8e96ce6a1eb94eb34a77f781
Mohamed Harby <mohamed.harby.ce@gmail.com>
2023-04-11 23:26:16 +0200

    fix: pic-name-changed

--- src/components/GallerySection/data.jsx
+++ src/components/GallerySection/data.jsx
@@ -1,118 +1,117 @@
-
 export const team = [
-    {
-        name:"Ahmed Rashed",
-        title:"head",
-        img:require("../../assets/img/team/ahmed-rashed.png")
-    },
-    {
-        name:"Talaat Taha",
-        title:"Mentor",
-        img:require("../../assets/img/team/talaat-taha.jpg")
-    },
-    {
-        name:"Mohab Ahmed",
-        title:"Mentor",
-        img:require("../../assets/img/team/mohab-ahmed.jpg")
-    },
-    {
-        name:"Nourhan Abosaeed",
-        title:"Mentor",
-        img:require("../../assets/img/profile.png")
-    },
-    {
-        name:"Ammar Younes",
-        title:"Mentor",
-        img:require("../../assets/img/team/ammar-younes.jpg")
-    },
-    {
-        name:"Bishoy Magdy",
-        title:"Mentor",
-        img:require("../../assets/img/team/bishoy-magdy.jpg")
-    },
-    {
-        name:"Mohamed Harby",
-        title:"Mentor",
-        img:require("../../assets/img/team/ahmed-harby.jpg")
-    },
-    {
-        name:"Abd El-Rahman Khattab",
-        title:"Mentor",
-        img:require("../../assets/img/team/abdelrahman-khattab.jpeg")
-    },
-    {
-        name:"Sara Hamza",
-        title:"Mentor",
-        img:require("../../assets/img/profile.png")
-    },
-    {
-        name:"Abd El-Rahman Elfeky",
-        title:"Mentor",
-        img:require("../../assets/img/team/elfeky.jpeg")
-    },
-    {
-        name:"Hazem Elakbawy",
-        title:"Mentor",
-        img:require("../../assets/img/team/hazem-elakabawy.jpg")
-    },
-    {
-        name:"Sayed Abd Elmongey",
-        title:"Mentor",
-        img:require("../../assets/img/team/sayed-abdelmongey.jpg")
-    },
-    {
-        name:"Mariam Kilany",
-        title:"Mentor",
-        img:require("../../assets/img/profile.png")
-    },
-    {
-        name:"Ahmed Mashaal",
-        title:"Mentor",
-        img:require("../../assets/img/team/ahmed-mashaal.jpg")
-    },
-    {
-        name:"Rahma Abo Eldahab",
-        title:"Mentor",
-        img:require("../../assets/img/profile.png")
-    },
-    {
-        name:"Hossam Eissa",
-        title:"Mentor",
-        img:require("../../assets/img/team/hossam-eissa.jpeg")
-    },
-    {
-        name:"Ahmed Tamer",
-        title:"Mentor",
-        img:require("../../assets/img/team/ahmed-tamer.jpg")
-    },
-    {
-        name:"Ayman Selim",
-        title:"Mentor",
-        img:require("../../assets/img/team/ayman-selim.jpg")
-    },
-    {
-        name:"Belal Ahmed",
-        title:"Mentor",
-        img:require("../../assets/img/team/belal-ahmed.jpg")
-    },
-    {
-        name:"Abd Allah Elagamy",
-        title:"Mentor",
-        img:require("../../assets/img/team/abdallah-elagamy.jpg")
-    },
-]
+  {
+    name: "Ahmed Rashed",
+    title: "head",
+    img: require("../../assets/img/team/ahmed-rashed.png"),
+  },
+  {
+    name: "Talaat Taha",
+    title: "Mentor",
+    img: require("../../assets/img/team/talaat-taha.jpg"),
+  },
+  {
+    name: "Mohab Ahmed",
+    title: "Mentor",
+    img: require("../../assets/img/team/mohab-ahmed.jpg"),
+  },
+  {
+    name: "Nourhan Abosaeed",
+    title: "Mentor",
+    img: require("../../assets/img/profile.png"),
+  },
+  {
+    name: "Ammar Younes",
+    title: "Mentor",
+    img: require("../../assets/img/team/ammar-younes.jpg"),
+  },
+  {
+    name: "Bishoy Magdy",
+    title: "Mentor",
+    img: require("../../assets/img/team/bishoy-magdy.jpg"),
+  },
+  {
+    name: "Mohamed Harby",
+    title: "Mentor",
+    img: require("../../assets/img/team/mohamed-harby.jpg"),
+  },
+  {
+    name: "Abd El-Rahman Khattab",
+    title: "Mentor",
+    img: require("../../assets/img/team/abdelrahman-khattab.jpeg"),
+  },
+  {
+    name: "Sara Hamza",
+    title: "Mentor",
+    img: require("../../assets/img/profile.png"),
+  },
+  {
+    name: "Abd El-Rahman Elfeky",
+    title: "Mentor",
+    img: require("../../assets/img/team/elfeky.jpeg"),
+  },
+  {
+    name: "Hazem Elakbawy",
+    title: "Mentor",
+    img: require("../../assets/img/team/hazem-elakabawy.jpg"),
+  },
+  {
+    name: "Sayed Abd Elmongey",
+    title: "Mentor",
+    img: require("../../assets/img/team/sayed-abdelmongey.jpg"),
+  },
+  {
+    name: "Mariam Kilany",
+    title: "Mentor",
+    img: require("../../assets/img/profile.png"),
+  },
+  {
+    name: "Ahmed Mashaal",
+    title: "Mentor",
+    img: require("../../assets/img/team/ahmed-mashaal.jpg"),
+  },
+  {
+    name: "Rahma Abo Eldahab",
+    title: "Mentor",
+    img: require("../../assets/img/profile.png"),
+  },
+  {
+    name: "Hossam Eissa",
+    title: "Mentor",
+    img: require("../../assets/img/team/hossam-eissa.jpeg"),
+  },
+  {
+    name: "Ahmed Tamer",
+    title: "Mentor",
+    img: require("../../assets/img/team/ahmed-tamer.jpg"),
+  },
+  {
+    name: "Ayman Selim",
+    title: "Mentor",
+    img: require("../../assets/img/team/ayman-selim.jpg"),
+  },
+  {
+    name: "Belal Ahmed",
+    title: "Mentor",
+    img: require("../../assets/img/team/belal-ahmed.jpg"),
+  },
+  {
+    name: "Abd Allah Elagamy",
+    title: "Mentor",
+    img: require("../../assets/img/team/abdallah-elagamy.jpg"),
+  },
+];
 
 export const community = [
-{
-    img:require("../../assets/img/community/community-pic-1.jpg")
-},
-{
-    img:require("../../assets/img/community/community-pic-2.jpg")
-},
-{
-    img:require("../../assets/img/community/community-pic-3.jpg")
-},
-{
-    img:require("../../assets/img/community/community-pic-4.jpg")
-},
-]
+  {
+    img: require("../../assets/img/community/community-pic-1.jpg"),
+  },
+  {
+    img: require("../../assets/img/community/community-pic-2.jpg"),
+  },
+  {
+    img: require("../../assets/img/community/community-pic-3.jpg"),
+  },
+  {
+    img: require("../../assets/img/community/community-pic-4.jpg"),
+  },
+];
